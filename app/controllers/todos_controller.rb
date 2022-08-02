1a61e20e99502d6d941c624bb1fd5b860c2f9d28
Kat Brandt <joyfulkat11@gmail.com>
2022-08-01 22:54:43 -0600

    Adds todos show route.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,14 @@ class TodosController < ApplicationController
     todos = Todo.all
     render json: todos
   end
+
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo
+
+  rescue ActiveRecord::RecordNotFound => e
+    render json: {
+      error: e.to_s
+    }, status: :not_found
+  end
 end
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -9,6 +9,31 @@ RSpec.describe "Todos API", type: :request do
 
     it 'returns todos' do
       expect(json).to_not be_empty
+      expect(json.size).to eq 10
+      expect(response.status).to eq 200
+      expect(response).to have_http_status 200
+      #above two test the same thing
+    end
+  end
+
+  describe "GET /todos/:id" do
+    before { get "/todos/#{todo_id}"}
+
+    context 'when the record exists' do
+      it 'returns the todo' do
+        expect(json).to_not be_empty
+        expect(json[:id]).to eq(todo_id)
+        expect(response.status).to eq 200
+      end
+    end
+
+    context 'when record does not exist' do
+      let(:todo_id) { 100 }
+
+      it 'returns status code 404 and error message' do
+        expect(response.status).to eq 404
+        expect(response.body).to match(/Couldn't find Todo/)
+      end
     end
   end
 end
